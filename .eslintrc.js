1f9466e813fa9e0d152f8bf9bc7fb9d9d2c331d8
will-ks <will@willks.com>
2023-02-03 19:41:57 +0100

    Add no-extraneous-dependencies rule and fix package.jsons

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,13 @@ module.exports = {
   extends: ['@redwoodjs/eslint-config'],
   root: true,
   rules: {
+    // Override extended configs - Should have justification
     'import/order': 'off', // Conflicts with Webstorm's optimize imports
     'prettier/prettier': 'off', // Annoying, handled by IDE
     '@typescript-eslint/no-unused-vars': 'off', // Annoying, handled by IDE
     '@typescript-eslint/no-non-null-assertion': 'off', // Occasionally useful
     'react/prop-types': 'off', // False positives, handled by TS
+    // Additional rules
+    'import/no-extraneous-dependencies': ['error', {}],
   },
 }
