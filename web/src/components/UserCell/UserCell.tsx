80e49a34dd0c4a50613205bfcc6e29c31e3b4044
will-ks <will@willks.com>
2023-02-28 13:50:35 +0100

    Add user cell

---
+++ web/src/components/UserCell/UserCell.tsx
@@ -0,0 +1,26 @@
+import type { CellFailureProps, CellSuccessProps } from '@redwoodjs/web'
+import { FC } from 'react'
+import ProfileCard from 'src/components/ProfileCard/ProfileCard'
+import type { UserQuery, UserQueryVariables } from 'types/graphql'
+
+export const QUERY = gql`
+  query UserQuery($id: String!) {
+    user: user(id: $id) {
+      username
+    }
+  }
+`
+
+export const Loading: FC = () => <div>Loading...</div>
+
+export const Empty: FC = () => <div>Empty</div>
+
+export const Failure: FC<CellFailureProps<UserQueryVariables>> = ({
+  error,
+}) => <div style={{ color: 'red' }}>Error: {error?.message}</div>
+
+export const Success: FC<CellSuccessProps<UserQuery, UserQueryVariables>> = ({
+  user: { username },
+}) => {
+  return <ProfileCard name={username ?? 'User'} />
+}
