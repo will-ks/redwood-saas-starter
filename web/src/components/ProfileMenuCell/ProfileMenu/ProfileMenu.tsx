f24aec273830599e568ed04342bd1a92e2a1a56b
will-ks <will@willks.com>
2023-03-08 16:24:03 +0100

    Use fullscreen menu on smaller screens

--- web/src/components/ProfileMenuCell/ProfileMenu/ProfileMenu.tsx
+++ web/src/components/ProfileMenuCell/ProfileMenu/ProfileMenu.tsx
@@ -1,32 +1,70 @@
-import { Avatar, Menu } from '@mantine/core'
+import { Avatar, Menu, NavLink, useMantineTheme } from '@mantine/core'
 import { navigate, routes } from '@redwoodjs/router'
-import { FC } from 'react'
+import { FC, ReactElement } from 'react'
 import { FiLogOut, FiSettings } from 'react-icons/fi'
+import { useMedia } from 'react-use'
 import { useAuth } from 'src/auth'
+import { openModal } from 'src/lib/overlays'
 
 const ProfileMenu: FC<{
   avatarSrc?: string
   name?: string
 }> = ({ avatarSrc, name }) => {
   const { logOut } = useAuth()
-  return (
+  const theme = useMantineTheme()
+  const isLargeScreen = useMedia(`(min-width: ${theme.breakpoints.sm}px)`)
+  const menuItems: { icon: ReactElement; name: string; onClick: () => void }[] =
+    [
+      {
+        icon: <FiSettings />,
+        name: 'Settings',
+        onClick: () => navigate(routes.settingsMenu()),
+      },
+      {
+        icon: <FiLogOut />,
+        name: 'Log out',
+        onClick: () => logOut(),
+      },
+    ]
+
+  return isLargeScreen ? (
     <Menu shadow="md" width={200} position={'top-end'}>
       <Menu.Target>
         <Avatar src={avatarSrc}>{name?.[0]}</Avatar>
       </Menu.Target>
 
       <Menu.Dropdown>
-        <Menu.Item
-          icon={<FiSettings />}
-          onClick={() => navigate(routes.settingsMenu())}
-        >
-          Settings
-        </Menu.Item>
-        <Menu.Item icon={<FiLogOut />} onClick={() => logOut()}>
-          Log out
-        </Menu.Item>
+        {menuItems.map((item) => (
+          <Menu.Item icon={item.icon} onClick={item.onClick} key={item.name}>
+            {item.name}
+          </Menu.Item>
+        ))}
       </Menu.Dropdown>
     </Menu>
+  ) : (
+    <button
+      onClick={() =>
+        openModal({
+          fullScreen: true,
+          title: 'Menu',
+          transition: 'pop',
+          children: (
+            <>
+              {menuItems.map((item) => (
+                <NavLink
+                  icon={item.icon}
+                  onClick={item.onClick}
+                  label={item.name}
+                  key={item.name}
+                />
+              ))}
+            </>
+          ),
+        })
+      }
+    >
+      <Avatar src={avatarSrc}>{name?.[0]}</Avatar>
+    </button>
   )
 }
 
